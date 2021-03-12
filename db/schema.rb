06d8a81f7ef4cdfa9382fd2500242a11a3f5a1b1
りか <rika@rikanoMacBook-Air.local>
2021-03-12 22:27:22 +0900

    Add Taskleaf

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_042628) do
+ActiveRecord::Schema.define(version: 2021_03_10_110329) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
