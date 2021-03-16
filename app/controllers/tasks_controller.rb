bb0894cbbf56b64a7479cde7a867b51904c7b29e
りか <rika@rikanoMacBook-Air.local>
2021-03-16 15:32:30 +0900

    Add Taskleaf

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
